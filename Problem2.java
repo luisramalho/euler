f533f1fd373c5d91e517e03fe1054e81d647a4dd
Luís Ramalho <info@luisramalho.com>
2014-09-09 09:14:57 +0300

    Format to standards

--- Problem2.java
+++ Problem2.java
@@ -6,9 +6,8 @@ package euler;
  * 
  * 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
  * 
- * By considering the terms in the Fibonacci sequence
- * whose values do not exceed four million, find the sum of the even-valued
- * terms.
+ * By considering the terms in the Fibonacci sequence whose values do not exceed
+ * four million, find the sum of the even-valued terms.
  */
 
 public class Problem2 {
@@ -17,15 +16,15 @@ public class Problem2 {
   public static void main(String[] args) {
     int i = 0;
     int sum = 0;
-    while(fib(i) <= MAX) {
+    while (fib(i) <= MAX) {
       if (fib(i) % 2 == 0) {
         sum += fib(i);
       }
       i++;
     }
-    System.out.print (sum);
+    System.out.print(sum);
   }
-  
+
   static long fib(int n) {
     if (n < 2) {
       return n;
--- Problem3.java
+++ Problem3.java
@@ -1,8 +1,8 @@
 package euler;
 
 /*
- * The prime factors of 13195 are 5, 7, 13 and 29.
- * What is the largest prime factor of the number 600851475143 ?
+ * The prime factors of 13195 are 5, 7, 13 and 29. What is the largest prime
+ * factor of the number 600851475143 ?
  */
 
 public class Problem3 {
@@ -10,12 +10,12 @@ public class Problem3 {
   public static void main(String[] args) {
     System.out.println(largestPrimeFactor(600851475143l));
   }
-  
+
   private static long largestPrimeFactor(long n) {
     int largest = 1;
-    for(int i = 2; i <= n; i++) {
-      while(n % i == 0) {
-        if(i > largest) {
+    for (int i = 2; i <= n; i++) {
+      while (n % i == 0) {
+        if (i > largest) {
           largest = i;
         }
         n /= i;
