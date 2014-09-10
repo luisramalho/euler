e9beb4b4efe360aaf20265cad1d60d992fc2cbaf
Luís Ramalho <info@luisramalho.com>
2014-09-10 17:42:13 +0300

    Solution for Problem #5

--- Problem5.java
+++ Problem5.java
@@ -1,5 +1,7 @@
 package euler;
 
+import java.util.ArrayList;
+
 /**
  * 2520 is the smallest number that can be divided by each of the numbers from 1
  * to 10 without any remainder. What is the smallest positive number that is
@@ -14,47 +16,75 @@ public final class Problem5 {
   /**
    * Private constructor.
    */
-  private Problem5() {
-    
-  }
-  
-  /**
-   * Highest divisor.
-   */
-  static final int HIGHEST_DIVISOR = 20;
-  
-  /**
-   * Lowest divisor.
-   */
-  static final int LOWEST_DIVISOR = 2;
+  private Problem5() { }
 
   /**
    * Main method.
    * @param args Arguments.
    */
   public static void main(final String[] args) {
-    for (int i = 21; i < Integer.MAX_VALUE; i++) {
-      if (evenlyDivisible(i)) {
-        System.out.println(i);
-        break;
+    double smallestMultiple = 1;
+    final int largestDivisor = 20;
+    final double limit = Math.sqrt(largestDivisor);
+    boolean check = true;
+    double[] a = new double[largestDivisor];
+    ArrayList<Integer> p = primeNumbers(largestDivisor * 2);
+
+    int i = 0;
+    while (p.get(i) <= largestDivisor) {
+      a[i] = 1;
+      if (check) {
+        if (p.get(i) <= limit) {
+          a[i] = Math.floor(Math.log(largestDivisor) / Math.log(p.get(i)));
+        } else {
+          check = false;
+        }
       }
+      smallestMultiple = smallestMultiple * Math.pow(p.get(i), a[i]);
+      i++;
+
     }
+    System.out.println((int) smallestMultiple);
   }
   
   /**
-   * Checks if the number can be divided by each of the numbers
-   * from LOWEST_DIVISOR to HIGHEST_DIVISOR without any remainder.
-   * @param number Number to be checked.
-   * @return <code>true</code> if number can be divided without any remainder;
-   *         <code>false</code> otherwise.
+   * Computes prime numbers.
+   * @param n   Ceiling of maximum prime number.
+   * @return    ArrayList of prime numbers.
    */
-  private static boolean evenlyDivisible(final int number) {
-    for (int i = LOWEST_DIVISOR; i <= HIGHEST_DIVISOR; i++) {
-      if (number % i != 0) {
+  private static ArrayList<Integer> primeNumbers(final int n) {
+    ArrayList<Integer> primes = new ArrayList<Integer>();
+    
+    for (int i = 0; i < n; i++) {
+      if (isPrime(i)) {
+        primes.add(i);
+      }
+    }
+    return primes;
+  }
+  
+  /**
+   * Checks if a number is prime.
+   * @param n  Number to be checked for primality.
+   * @return   <code>true</code> if the number is prime;
+   *           <code>false</code> otherwise.
+   */
+  private static boolean isPrime(final int n) {
+    if (n < 2) {
+      return false;
+    }
+    
+    if (n == 2) {
+      return true;
+    }
+    
+    for (int i = 2; i < Math.sqrt(n) + 1; i++) {
+      if (n % i == 0) {
         return false;
       }
     }
+
     return true;
   }
-
-}
+  
+}  
