97b824bcd4fa0d722b23d94688cff0aeda1c2c30
Luís Ramalho <info@luisramalho.com>
2014-09-17 12:31:03 +0300

    Solution for Problem #10 (using The  sieve of Eratosthenes)

--- Problem010.java
+++ Problem010.java
@@ -1,5 +1,7 @@
 package euler;
 
+import java.util.Arrays;
+
 /**
  * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
  * 
@@ -13,7 +15,13 @@ public final class Problem010 {
     /**
      * Max prime number to check.
      */
-    static final int MAX_PRIME = 2_000_000;
+    static final int LIMIT = 2_000_000;
+
+    /**
+     * Cross limit, because any smaller multiple of a next number p that has a
+     * divisor less than p has already been crossed out as a multiple of that.
+     */
+    static final double CROSS_LIMIT = Math.sqrt(LIMIT);
 
     /**
      * Private constructor.
@@ -28,66 +36,29 @@ public final class Problem010 {
      *            Arguments.
      */
     public static void main(final String[] args) {
-        long sum = 5;
-        int i = 5;
-        while (i <= MAX_PRIME) {
-            if (isPrime(i)) {
-                sum += i;
-            }
-            i += 2;
+        boolean[] sieve = new boolean[LIMIT];
+        Arrays.fill(sieve, false);
 
-            if (i < MAX_PRIME && isPrime(i)) {
+        for (int i = 4; i < LIMIT; i += 2) {
+            sieve[i] = true;
+        }
+
+        for (int i = 3; i < CROSS_LIMIT; i += 2) {
+            if (!sieve[i]) {
+                for (int j = i * i; j < LIMIT; j += 2 * i) {
+                    sieve[j] = true;
+                }
+            }
+        }
+
+        long sum = 0;
+
+        for (int i = 2; i < LIMIT; i++) {
+            if (!sieve[i]) {
                 sum += i;
             }
-            i += 4;
         }
+
         System.out.println(sum);
-    }
-
-    /**
-     * Checks if a number is prime.
-     * 
-     * @param n
-     *            Number to be checked for primality.
-     * @return <code>true</code> if the number is prime; <code>false</code>
-     *         otherwise.
-     */
-    private static boolean isPrime(final int n) {
-        if (n == 1) {
-            return false;
-        }
-
-        if (n < 4) {
-            return true;
-        }
-
-        if (n % 2 == 0) {
-            return false;
-        }
-
-        if (n < 9) {
-            return true;
-        }
-
-        if (n % 3 == 0) {
-            return false;
-        }
-
-        double r = Math.floor(Math.sqrt(n));
-        int f = 5;
-
-        while (f <= r) {
-            if (n % f == 0) {
-                return false;
-            }
-
-            if (n % (f + 2) == 0) {
-                return false;
-            }
-
-            f += 6;
-        }
-
-        return true;
     }
 }
