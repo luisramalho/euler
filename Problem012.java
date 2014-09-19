c103e5d0a118232ea73f272a8788352e8e5981db
Luís Ramalho <info@luisramalho.com>
2014-09-19 20:55:10 +0300

    Solution for Problem #12 (improved++)

--- Problem012.java
+++ Problem012.java
@@ -14,6 +14,11 @@ public final class Problem012 {
     static final int LIMIT = 500;
 
     /**
+     * Primes.
+     */
+    static final int P = 10000;
+
+    /**
      * Private constructor.
      */
     private Problem012() {
@@ -30,23 +35,98 @@ public final class Problem012 {
         int t = 1;
         int a = 1;
         int count = 0;
+        int tt, i, exponent;
+        int[] primes = primes(P);
         while (count <= LIMIT) {
-            count = 0;
+            count = 1;
             a += 1;
             t += a;
-            int ttx = (int) Math.sqrt(t);
-            for (int i = 1; i <= ttx; i++) {
-                if (t % i == 0) {
-                    count += 2;
+            tt = t;
+            for (i = 1; i <= P; i++) {
+                if (primes[i] * primes[i] > tt) {
+                    count *= 2;
+                    break;
                 }
 
-                // correction for a perfect square
-                if (t == ttx * ttx) {
-                    count--;
+                exponent = 1;
+                while (tt % primes[i] == 0) {
+                    exponent++;
+                    tt /= primes[i];
+                }
+                if (exponent > 1) {
+                    count *= exponent;
+                }
+                if (tt == 1) {
+                    break;
                 }
             }
         }
         System.out.println(t);
     }
 
+    /**
+     * Calculates primes.
+     *
+     * @param n
+     *            Max prime.
+     * @return array of primes.
+     */
+    private static int[] primes(final int n) {
+        int[] p = new int[n];
+        int count = 1;
+        for (int i = 1; i <= n; i++) {
+            if (isPrime(i)) {
+                p[count] = i;
+                count++;
+            }
+        }
+        return p;
+    }
+
+    /**
+     * Checks if a number is prime.
+     *
+     * @param n
+     *            Number to be checked for primality.
+     * @return <code>true</code> if the number is prime; <code>false</code>
+     *         otherwise.
+     */
+    private static boolean isPrime(final int n) {
+        if (n == 1) {
+            return false;
+        }
+
+        if (n < 4) {
+            return true;
+        }
+
+        if (n % 2 == 0) {
+            return false;
+        }
+
+        if (n < 9) {
+            return true;
+        }
+
+        if (n % 3 == 0) {
+            return false;
+        }
+
+        double r = Math.floor(Math.sqrt(n));
+        int f = 5;
+
+        while (f <= r) {
+            if (n % f == 0) {
+                return false;
+            }
+
+            if (n % (f + 2) == 0) {
+                return false;
+            }
+
+            f += 6;
+        }
+
+        return true;
+    }
 }
