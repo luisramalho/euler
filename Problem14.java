a39b21a7724654181cf850ad092bdbb83cadf22d
Luís Ramalho <info@luisramalho.com>
2014-09-23 14:57:49 +0300

    Solution for Problem #14 (optimized ~10x faster)

--- Problem14.java
+++ Problem14.java
@@ -9,6 +9,11 @@ package euler;
 public final class Problem14 {
 
     /**
+     * Highest number to check.
+     */
+    static final int LIMIT = 1_000_000;
+
+    /**
      * Private constructor.
      */
     private Problem14() {
@@ -22,28 +27,43 @@ public final class Problem14 {
      *            Arguments.
      */
     public static void main(final String[] args) {
-        int n = 0;
-        int longestChain = 0;
-        int count = 0;
+        long start = System.currentTimeMillis();
         long current = 0;
+        int longestChain = 0;
+        int chainLength = 0;
         int startingNumber = 0;
-        for (int i = 1; i < 1_000_000; i++) {
-            count = 0;
+        boolean[] checked = new boolean[LIMIT];
+        int[] chainsLength = new int[LIMIT];
+        int tempStartingNumber = 0;
+        for (int i = 1; i < LIMIT; i++) {
             current = i;
-            startingNumber = i;
+            chainLength = 0;
+            tempStartingNumber = i;
             do {
                 if (current % 2 == 0) {
                     current /= 2;
                 } else {
                     current = 3 * current + 1;
                 }
-                count++;
+                chainLength++;
+
+                // Caching and checking for lower chains already solved.
+                if (current < tempStartingNumber && checked[(int) current]) {
+                    chainLength += chainsLength[(int) current];
+                    chainsLength[tempStartingNumber] = chainLength;
+                    current = 1;
+                }
             } while (current > 1);
-            if (count > longestChain) {
-                longestChain = count;
-                n = startingNumber;
+
+            checked[tempStartingNumber] = true;
+
+            if (chainLength > longestChain) {
+                longestChain = chainLength;
+                startingNumber = tempStartingNumber;
             }
         }
-        System.out.println(n + " (" + (longestChain + 1) + ")");
+        long end = System.currentTimeMillis();
+        System.out.println(startingNumber + " (" + (longestChain + 1) + ")");
+        System.out.println("Took: " + (end - start) + "ms");
     }
 }
