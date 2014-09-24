0f62963b082dba25f31a49b18c12a58c024a9a5f
Luís Ramalho <info@luisramalho.com>
2014-09-24 11:43:40 +0300

    Added the iterative solution

--- Problem015.java
+++ Problem015.java
@@ -27,11 +27,46 @@ public final class Problem015 {
      *            Arguments.
      */
     public static void main(final String[] args) {
+        System.out.println(binomialCoefficient(GRID_SIZE));
+        System.out.println(iterativeSolution(GRID_SIZE));
+    }
+
+    /**
+     * Binomial coefficient solution.
+     *
+     * @param size
+     *            size of the grid.
+     * @return lattice paths.
+     */
+    private static long binomialCoefficient(final int size) {
         long count = 1;
-        for (int i = 0; i < GRID_SIZE; i++) {
-            count = (count * (2 * GRID_SIZE - i)) / (i + 1);
+        for (int i = 0; i < size; i++) {
+            count = (count * (2 * size - i)) / (i + 1);
         }
-        System.out.println(count);
+        return count;
+    }
+
+    /**
+     * Iterative solution.
+     *
+     * @param size
+     *            size of the grid.
+     * @return lattice paths.
+     */
+    private static long iterativeSolution(final int size) {
+        long[][] grid = new long[size + 1][size + 1];
+
+        for (int i = 0; i <= size; i++) {
+            grid[i][0] = 1;
+            grid[0][i] = 1;
+        }
+
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
+            }
+        }
+        return grid[size][size];
     }
 
 }
