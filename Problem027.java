61cd8347f94a298e7156dc3984bd325826eb828b
Luís Ramalho <info@luisramalho.com>
2016-08-20 12:13:32 +0300

    Update Copyright

--- Problem027.java
+++ Problem027.java
@@ -1,7 +1,7 @@
 /*
- * The MIT License
+ * The MIT License (MIT)
  *
- * Copyright 2014 luisramalho.
+ * Copyright (c) 2016 Luís Ramalho
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
--- Problem049.java
+++ Problem049.java
@@ -1,9 +1,9 @@
 /*
- * The MIT License
+ * The MIT License (MIT)
  *
- * Copyright 2014 luisramalho.
+ * Copyright (c) 2016 Luís Ramalho
  *
- * Permission is hereby granted, free of charge, to any person obtaining numbers copy
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
